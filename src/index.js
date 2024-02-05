92261cdea6dd58f7cba10143b144572138b1b0e9
Molzinho2 <igor.mol2013@gmail.com>
2024-02-04 22:42:55 -0300

    renamed:    public/data.js -> src/data.js 	modified:   src/index.css 	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,33 +1,53 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { pizzaData } from './data';
+import "./index.css"
 
 function App(){
     return (
-    <div>
+    <div className='container'>
         <Header/>
-        <Pizza/>
-        <Pizza/>
-        <Pizza/>
+        <Menu/>
         <Footer/>
     </div>
     );
 }
 
 function Header(){
-    return <h1> Fast React Pizza Co.</h1>;
-};
+    const style = {};
+
+    return (
+        <header className='header'>
+                <h1 style={style}> Fast React Pizza Co.</h1>;
+        </header>
+)};
 
 function Menu(){
-    return <h2>Our Menu</h2>
-};
+    return (
+    <main className='menu'>
+        <h2>Our Menu</h2>
+        <Pizza name={pizzaData[2].name} ingredient={pizzaData[2].ingredients} price={12} photoname='pizzas/spinaci.jpg' />
+        <Pizza name={pizzaData[0].name} ingredient={pizzaData[0].ingredients} price={10} photoname='pizzas/focaccia.jpg' />
+        <Pizza name={pizzaData[1].name} ingredient={pizzaData[1].ingredients} price={15} photoname='pizzas/margherita.jpg' />
+        <Pizza name={pizzaData[3].name} ingredient={pizzaData[3].ingredients} price={20} photoname='pizzas/funghi.jpg' />
+    </main>
+)};
 
 function Footer(){
-    return <footer>{new Date().toLocaleTimeString()} We're currently open!</footer>
+    return <footer className='footer'>{new Date().toLocaleTimeString()} We're currently open!</footer>
 };
 
-function Pizza(){
-    return <h2>Pizza</h2>
-};
+function Pizza(props){
+    return (
+    <div className='pizza'>
+        <img src={props.photoname}  alt={props.name}/>
+        <div>
+            <h3>{props.name}</h3>
+            <p>{props.ingredient}</p>
+            <span>{props.price}</span>
+        </div>
+    </div>
+)};
 
 const root = createRoot(document.getElementById('root'));
 root.render(
